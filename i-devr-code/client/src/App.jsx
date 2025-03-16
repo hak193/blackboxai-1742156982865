d6634aa86033e7172b2085cee7e27fcb0704185d
black hat developer <idevr@icloud.com>
2025-03-16 16:29:51 -0400

    Add i-devr-code/client/src/App.jsx 1742156990986

---
+++ i-devr-code/client/src/App.jsx
@@ -0,0 +1,11 @@
+import PaymentForm from './components/PaymentForm'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <PaymentForm />
+    </div>
+  )
+}
+
+export default App
